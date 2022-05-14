7e182883b8d0a8ec31a491b483c272de9210575b
aselwilliams <aselwilliams22@gmail.com>
2022-05-14 08:00:26 -0400

    ms, s, min are displayed

--- src/App.js
+++ src/App.js
@@ -22,7 +22,9 @@ function App() {
   return (
     <div className="App">
       <div>
-        <span>{(time / 10) % 100}</span>
+        <span>{('0' + Math.floor((time / 60000) % 60)).slice(-2)} :</span>
+        <span>{('0' + Math.floor((time / 1000) % 60)).slice(-2)} :</span>
+        <span>{('0' + ((time / 10) % 100)).slice(-2)}</span>
       </div>
       <div>
         <button onClick={()=>setTimerOn(true)}>Start</button>
