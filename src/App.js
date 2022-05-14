109cdb30c41e272bed7f3401977f4c010a624506
aselwilliams <aselwilliams22@gmail.com>
2022-05-14 08:08:49 -0400

    conditional rendering added on buttons

--- src/App.js
+++ src/App.js
@@ -27,10 +27,19 @@ function App() {
         <span>{('0' + ((time / 10) % 100)).slice(-2)}</span>
       </div>
       <div>
-        <button onClick={()=>setTimerOn(true)}>Start</button>
-        <button onClick={()=>setTimerOn(false)}>Stop</button>
-        <button onClick={()=>setTimerOn(true)}>Resume</button>
-        <button onClick={()=>setTime(0)}>Reset</button>
+        {!timerOn && time===0 && (
+          <button onClick={()=>setTimerOn(true)}>Start</button>
+        )}
+        {timerOn && (
+          <button onClick={()=>setTimerOn(false)}>Stop</button>
+        )}
+        {!timerOn && time!==0 && (
+          <button onClick={()=>setTimerOn(true)}>Resume</button>
+        )}
+        {!timerOn && time>0 && (
+          <button onClick={()=>setTime(0)}>Reset</button>
+
+        )}
       </div>
     </div>
   );
