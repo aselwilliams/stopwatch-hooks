927c17e0157da198f463ad86b17dca17f7bdc691
aselwilliams <aselwilliams22@gmail.com>
2022-05-14 08:53:05 -0400

    last touches

--- src/App.js
+++ src/App.js
@@ -24,8 +24,8 @@ function App() {
     <div className="card__box">
       <h1>Stopwatch</h1>
       <div className="time">
-        <span>{('0' + Math.floor((time / 60000) % 60)).slice(-2)} :</span>
-        <span>{('0' + Math.floor((time / 1000) % 60)).slice(-2)} :</span>
+        <span>{('0' + Math.floor((time / 60000) % 60)).slice(-2)} : </span>
+        <span>{('0' + Math.floor((time / 1000) % 60)).slice(-2)} : </span>
         <span>{('0' + ((time / 10) % 100)).slice(-2)}</span>
       </div>
       <div>
